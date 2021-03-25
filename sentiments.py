dd1d21cf6193d9a52664915978ee4d0f04aaa469
Waqar Ahmad Khan <waqaribnmuhammad@gmail.com>
2021-03-25 20:43:50 +0500

    Configured API
    Added Twitter API to extract tweets.

---
+++ sentiments.py
@@ -0,0 +1,26 @@
+import tweepy
+from textblob import TextBlob
+import twitterapi
+
+
+"""
+# Remove comments and add your api key.
+# Also remove twitterapi import.
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token_secret = ''
+"""
+
+auth = tweepy.OAuthHandler(twitterapi.consumer_key, twitterapi.consumer_secret)
+auth.set_access_token(twitterapi.access_token, twitterapi.access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('#Covid19')
+
+for tweet in public_tweets:
+	#print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	print(analysis)
